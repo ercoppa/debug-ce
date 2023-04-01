50e229ca7ce2202eeb908a0ffa2734258a26859f
Emilio Coppa <ercoppa@gmail.com>
2023-04-01 16:19:41 +0000

    microbenchmarks

--- tests/microbenchmarks/symqemu/execute.sh
+++ tests/microbenchmarks/symqemu/execute.sh
@@ -6,3 +6,5 @@ rm -rf $SYMCC_OUTPUT_DIR || echo "Nothing to clean"
 mkdir $SYMCC_OUTPUT_DIR
 # -d in_asm,op_opt
 SYMCC_INPUT_FILE=input.dat ../../../../symqemu/x86_64-linux-user/symqemu-x86_64 ./main.symqemu input.dat # 2> qemu.log
+# echo run ./main.symqemu input.dat
+# SYMCC_INPUT_FILE=input.dat gdb ../../../../symqemu/x86_64-linux-user/symqemu-x86_64
